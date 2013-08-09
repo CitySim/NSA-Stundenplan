4370e8c6122374402b6bb466e5c5422d78f11364
it1-markde <it1-markde@R053-07.g18.de>
2013-08-09 11:30:19 +0200

    +

--- server/src/test/server/persistance/HibernateTest.java
+++ server/src/test/server/persistance/HibernateTest.java
@@ -1,8 +1,8 @@
 package server.persistance;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
-import java.util.Date;
 import java.util.List;
 
 import javax.persistence.EntityManager;
@@ -11,15 +11,9 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import server.persistance.HibernateUtil;
+import server.persistence.HibernateUtil;
 import storage.entities.Klasse;
 import storage.entities.Lehrer;
-import storage.entities.Raum;
-import storage.entities.Stunde;
-import storage.entities.StundenplanStunde;
-import storage.entities.Tag;
-import storage.entities.Unterrichtsfach;
-import storage.entities.Vertretung;
 
 public class HibernateTest {
 
@@ -27,73 +21,75 @@ public class HibernateTest {
 
 	@Before
 	public void init() {
-		em = HibernateUtil.getEntityManager();
+		this.em = new HibernateUtil().getEntityManager();
 	}
 
 	@After
 	public void cleanup() {
-		em.close();
+		this.em.close();
 	}
 
 	@Test
 	public void entityManagerTest() {
-		em.getTransaction().begin();
-		Klasse klasse1 = new Klasse();
+		this.em.getTransaction().begin();
+		final Klasse klasse1 = new Klasse();
 		klasse1.setBezeichung("ita");
-		Klasse klasse2 = new Klasse();
+		final Klasse klasse2 = new Klasse();
 		klasse2.setBezeichung("itb");
-		em.persist(klasse1);
-		em.persist(klasse2);
-		em.getTransaction().commit();
-		
-		final List<Klasse> list = em.createQuery("select p from Klasse p").getResultList();
+		this.em.persist(klasse1);
+		this.em.persist(klasse2);
+		this.em.getTransaction().commit();
+
+		final List<Klasse> list = this.em.createQuery("select p from Klasse p")
+				.getResultList();
 		assertEquals(2, list.size());
-		for (Klasse current : list) {
+		for (final Klasse current : list) {
 			final String firstName = current.getBezeichung();
 			assertTrue(firstName.equals("ita") || firstName.equals("itb"));
 		}
 	}
-	
-	@Test
-	public void stundenplanStundeTest(){
-		em.getTransaction().begin();
 
-		Klasse klasse1 = new Klasse();
+	@Test
+	public void stundenplanStundeTest() {
+		this.em.getTransaction().begin();
+
+		final Klasse klasse1 = new Klasse();
 		klasse1.setBezeichung("ita");
-		
-		Lehrer lehrer1 = new Lehrer();
+
+		final Lehrer lehrer1 = new Lehrer();
 		lehrer1.setKurzName("Hr");
 		lehrer1.setName("Hermann");
 		lehrer1.setVorname("Werner");
-		
-//		Raum raum = new Raum();
-//		raum.setBezeichnung("53");
-//		
-//		Unterrichtsfach fach = new Unterrichtsfach();
-//		fach.setBezeichnung("Wirtschaft und Gesellschaft");
-//		fach.setKurzName("WuG");
-//		
-//		Tag tag = new Tag();
-//		tag.setBezeichnung("Montag");
-//		
-//		Stunde stunde = new Stunde();
-//		stunde.setZeitBis(new Date());
-//		stunde.setZeitVon(new Date());
-//		
-//		Vertretung vertretung = new Vertretung();
-//		vertretung.setDatum(new Date());
 
-//		StundenplanStunde plan = new StundenplanStunde();
-//		plan.setKlasse(klasse1);
-//		plan.setLehrer(lehrer1);
-//		em.persist(plan);
+		// Raum raum = new Raum();
+		// raum.setBezeichnung("53");
+		//
+		// Unterrichtsfach fach = new Unterrichtsfach();
+		// fach.setBezeichnung("Wirtschaft und Gesellschaft");
+		// fach.setKurzName("WuG");
+		//
+		// Tag tag = new Tag();
+		// tag.setBezeichnung("Montag");
+		//
+		// Stunde stunde = new Stunde();
+		// stunde.setZeitBis(new Date());
+		// stunde.setZeitVon(new Date());
+		//
+		// Vertretung vertretung = new Vertretung();
+		// vertretung.setDatum(new Date());
 
-		em.persist(klasse1);
-		em.persist(lehrer1);
+		// StundenplanStunde plan = new StundenplanStunde();
+		// plan.setKlasse(klasse1);
+		// plan.setLehrer(lehrer1);
+		// em.persist(plan);
 
-		em.getTransaction().commit();
-		
-		final List<Klasse> list = em.createQuery("select p from Klasse p").getResultList();
+		this.em.persist(klasse1);
+		this.em.persist(lehrer1);
+
+		this.em.getTransaction().commit();
+
+		final List<Klasse> list = this.em.createQuery("select p from Klasse p")
+				.getResultList();
 		assertEquals(1, list.size());
 	}
 }
