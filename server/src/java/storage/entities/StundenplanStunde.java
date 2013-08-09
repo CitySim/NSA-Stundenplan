4161406eda4ca00888120d37dfc8b6b91496ce68
oleg-scheltow <olegscheltow@yahoo.de>
2013-08-09 08:37:27 +0200

    Generated Getters and Setters for StundenplanStunde

--- server/src/java/storage/entities/StundenplanStunde.java
+++ server/src/java/storage/entities/StundenplanStunde.java
@@ -12,41 +12,98 @@ public class StundenplanStunde extends DefaultEntity{
                 insertable=false, updatable=false, 
                 nullable=false)
 	private Klasse klasse;
+   
+    public Klasse getKlasse() {
+		return klasse;
+	}
 
-    @ManyToOne
+	public void setKlasse(Klasse klasse) {
+		this.klasse = klasse;
+	}
+
+	@ManyToOne
     @JoinColumn(name="id", 
                 insertable=false, updatable=false, 
                 nullable=false)
 	private Tag tag;
     
-    @ManyToOne
+    public Tag getTag() {
+		return tag;
+	}
+
+	public void setTag(Tag tag) {
+		this.tag = tag;
+	}
+
+	@ManyToOne
     @JoinColumn(name="id", 
                 insertable=false, updatable=false, 
                 nullable=false)
 	private Stunde stunde;
     
-    @ManyToOne
+    public Stunde getStunde() {
+		return stunde;
+	}
+
+	public void setStunde(Stunde stunde) {
+		this.stunde = stunde;
+	}
+
+	@ManyToOne
     @JoinColumn(name="id", 
                 insertable=false, updatable=false, 
                 nullable=false)
 	private Lehrer lehrer;
     
-    @ManyToOne
+	
+    public Lehrer getLehrer() {
+		return lehrer;
+	}
+
+	public void setLehrer(Lehrer lehrer) {
+		this.lehrer = lehrer;
+	}
+
+	@ManyToOne
     @JoinColumn(name="id", 
                 insertable=false, updatable=false, 
                 nullable=false)
 	private Unterrichtsfach unterrichtsfach;
     
-    @ManyToOne
+	public Unterrichtsfach getUnterrichtsfach() {
+		return unterrichtsfach;
+	}
+
+	public void setUnterrichtsfach(Unterrichtsfach unterrichtsfach) {
+		this.unterrichtsfach = unterrichtsfach;
+	}
+
+	@ManyToOne
     @JoinColumn(name="id", 
                 insertable=false, updatable=false, 
                 nullable=false)
 	private Raum raum;
+	
+    public Raum getRaum() {
+		return raum;
+	}
+
+	public void setRaum(Raum raum) {
+		this.raum = raum;
+	}
     
     @ManyToOne
     @JoinColumn(name="id", 
                 insertable=false, updatable=false, 
                 nullable=false)
 	private Vertretung vertretung;
+
+	public Vertretung getVertretung() {
+		return vertretung;
+	}
+
+	public void setVertretung(Vertretung vertretung) {
+		this.vertretung = vertretung;
+	}
 
 }
