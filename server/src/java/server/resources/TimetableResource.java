a35226a6423b5df38e1c9abdbcf4f1c45512805c
leonard- <wark@freenet.de>
2013-08-15 14:45:54 +0200

    api: TODO timetable

--- server/src/java/server/resources/TimetableResource.java
+++ server/src/java/server/resources/TimetableResource.java
@@ -1,5 +1,34 @@
 package server.resources;
 
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import server.entities.Timetable;
+import server.persistence.HibernateUtil;
+
+import com.google.gson.Gson;
+
+// TODO
+@Path("timetable")
 public class TimetableResource {
 
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public String getTimetablesJSON() {
+		final Gson gson = new Gson();
+		final String json = gson.toJson(getTimetables());
+		return json;
+	}
+
+	@SuppressWarnings("unchecked")
+	public List<Timetable> getTimetables() {
+		return HibernateUtil
+				.getEntityManager()
+				.createNativeQuery("select * from Tag",
+						Timetable.class).getResultList();
+	}
 }
