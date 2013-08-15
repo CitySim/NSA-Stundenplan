b1e8a39a8f2039d734a872ca9e6be56305f47bcd
Oleg Scheltow <olegscheltow@yahoo.de>
2013-08-15 10:47:02 +0200

    Added getForms

--- server/src/java/server/resources/FormResource.java
+++ server/src/java/server/resources/FormResource.java
@@ -18,11 +18,16 @@ public class FormResource {
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
 	public String getFormsJSON() {
-		final List<Form> list = HibernateUtil.getEntityManager()
-				.createNativeQuery("select * from Klasse", Form.class)
-				.getResultList();
+		final List<Form> list = getForms();
 		final Gson gson = new Gson();
 		final String json = gson.toJson(list);
 		return json;
 	}
+
+	public List<Form> getForms() {
+		return HibernateUtil.getEntityManager()
+				.createNativeQuery("select * from Klasse", Form.class)
+				.getResultList();
+	}
+
 }
