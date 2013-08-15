bfd4d9d7ebd7cd11500bf53307b4a6a568f11f59
Oleg Scheltow <olegscheltow@yahoo.de>
2013-08-15 13:26:13 +0200

    Add create User and changed SQL Queries

--- server/src/java/server/queries/LoginQuery.java
+++ server/src/java/server/queries/LoginQuery.java
@@ -2,6 +2,8 @@ package server.queries;
 
 import java.util.List;
 
+import javax.persistence.Query;
+
 import server.entities.Login;
 import server.persistence.HibernateUtil;
 
@@ -9,6 +11,7 @@ public class LoginQuery {
 	String password = null;
 	
 	public String getPassword(String username){
+		
 		List<Login> loginList =  HibernateUtil.getEntityManager()
 					.createNativeQuery("select * from Login", Login.class)
 					.getResultList();
@@ -18,6 +21,11 @@ public class LoginQuery {
 			}
 		}
 		return password;
+	}
+	
+	public void setUser(String username,String password){
+		Query  persistableQuery = HibernateUtil.getEntityManager().createNativeQuery("INSERT INTO `login`(`password`, `user`) VALUES ("+password+","+username+")", Login.class);
+		persistableQuery.executeUpdate();
 	}
 	
 }
