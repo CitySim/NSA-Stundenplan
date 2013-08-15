9a22c0cd0e9e9579cedf713e723b611859701998
Oleg Scheltow <olegscheltow@yahoo.de>
2013-08-15 11:20:58 +0200

    Correct Login Query

--- server/src/java/server/queries/LoginQuery.java
+++ server/src/java/server/queries/LoginQuery.java
@@ -6,13 +6,18 @@ import server.entities.Login;
 import server.persistence.HibernateUtil;
 
 public class LoginQuery {
+	String password = null;
 	
 	public String getPassword(String username){
 		List<Login> loginList =  HibernateUtil.getEntityManager()
 					.createNativeQuery("select * from Login", Login.class)
 					.getResultList();
-		return null;
-
+		for (Login login : loginList) {
+			if(username.equals(login.getUser())){
+				password = login.getPassword();
+			}
+		}
+		return password;
 	}
 	
 }
