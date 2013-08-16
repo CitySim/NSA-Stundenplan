984b0017943fe4ddf88a11f604e87ccc33148443
it1-markde <it1-markde@R053-07.g18.de>
2013-08-16 08:47:12 +0200

    Completed cookie creation and check.

--- server/src/java/server/operations/CookieServlet.java
+++ server/src/java/server/operations/CookieServlet.java
@@ -1,35 +1,41 @@
 package server.operations;
 
-import java.io.IOException;
-
-import javax.servlet.ServletException;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 public class CookieServlet {
 
-	public void doGet(final String userName, final HttpServletResponse response)
-			throws ServletException, IOException {
+	public void doGet(final String userName, final HttpServletResponse response) {
 
-		final Cookie cookie = new Cookie("NSA_Cookie", "IDTest");
-		cookie.setValue(new PasswordEncryptor().generateEncryptedPassword());
+		final String cookieID = new PasswordEncryptor()
+				.generateEncryptedPassword();
+		final Cookie cookie = new Cookie("NSA_Cookie", cookieID);
+		cookie.setValue(cookieID);
 		cookie.setMaxAge(600);
 		// TODO: save password in the DB / temp storage
 		response.addCookie(cookie);
 	}
 
-	public boolean validateKey(final HttpServletRequest req) {
+	public boolean validateKey(final HttpServletRequest request) {
 
-		// get the attribute
-		final String clientKey = (String) req.getAttribute("");
-		// TODO: Check the DB for the key
-		final String dbKey = null;
+		final Cookie[] cookies = request.getCookies();
+		String clientKey = "";
+		boolean valid = false;
+		if (cookies != null) {
+			for (final Cookie cookie : cookies) {
+				if ("NSA_Cookie".equals(cookie.getName())) {
+					clientKey = cookie.getValue();
+				}
+			}
+			// TODO: Check the DB for the key
+			final String dbKey = null;
 
-		if (clientKey.equals(dbKey)) {
-			return true;
+			if (clientKey.equals(dbKey)) {
+				valid = true;
+			}
 		}
-		return false;
-
+		return valid;
 	}
+
 }
