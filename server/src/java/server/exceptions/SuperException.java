c61b34e5fa91175146a64c55e25f2a85ce3d5ff9
it1-markde <it1-markde@R053-07.g18.de>
2013-08-20 11:28:07 +0200

    Added loggerTest.

--- server/src/java/server/exceptions/SuperException.java
+++ server/src/java/server/exceptions/SuperException.java
@@ -3,7 +3,8 @@ package server.exceptions;
 import java.util.Date;
 
 /**
- * Used as super class for other exceptions to implement the showErrorMessage method.
+ * Used as super class for other exceptions to implement the showErrorMessage
+ * method.
  * 
  * @author dennis.markmann
  * @since JDK.1.7.0_25
@@ -12,40 +13,41 @@ import java.util.Date;
 
 public class SuperException extends Exception {
 
-    private static final long serialVersionUID = 708521289592050569L;
+	private static final long serialVersionUID = 708521289592050569L;
 
-    private int errorNumber = 0;
-    private String errorTitel = "";
-    private String errorMessage = "";
-    private Date date = null;
+	private int errorNumber = 0;
+	private String errorTitel = "";
+	private String errorMessage = "";
+	private Date date = null;
 
-    SuperException(final int errorNumber, final String errorTitel, final String errorMessage) {
+	public SuperException(final int errorNumber, final String errorTitel,
+			final String errorMessage) {
 
-        this.errorNumber = errorNumber;
-        this.errorTitel = errorTitel;
-        this.errorMessage = errorMessage;
-        this.date = new Date();
-    }
+		this.errorNumber = errorNumber;
+		this.errorTitel = errorTitel;
+		this.errorMessage = errorMessage;
+		this.date = new Date();
+	}
 
-    public String createLogingMessage() {
-        final StringBuffer sb = new StringBuffer();
-        sb.append(this.date);
-        sb.append(": ErrorNumber'");
-        sb.append(this.errorNumber);
-        sb.append("' Titel'");
-        sb.append(this.errorTitel);
-        sb.append("'");
-        return sb.toString();
-    }
+	public String createLogingMessage() {
+		final StringBuffer sb = new StringBuffer();
+		sb.append(this.date);
+		sb.append(": ErrorNumber'");
+		sb.append(this.errorNumber);
+		sb.append("' Titel'");
+		sb.append(this.errorTitel);
+		sb.append("'");
+		return sb.toString();
+	}
 
-    public String showErrorMessage() {
-        final StringBuffer sb = new StringBuffer();
-        sb.append(this.errorNumber);
-        sb.append(": '");
-        sb.append(this.errorTitel);
-        sb.append("' '");
-        sb.append(this.errorMessage);
-        sb.append("'");
-        return sb.toString();
-    }
+	public String showErrorMessage() {
+		final StringBuffer sb = new StringBuffer();
+		sb.append(this.errorNumber);
+		sb.append(": '");
+		sb.append(this.errorTitel);
+		sb.append("' '");
+		sb.append(this.errorMessage);
+		sb.append("'");
+		return sb.toString();
+	}
 }
---
+++ server/src/test/server/operations/ExceptionLoggerTest.java
@@ -0,0 +1,34 @@
+package server.operations;
+
+import junit.framework.TestCase;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import server.exceptions.SuperException;
+
+/**
+ * Test for exception logging.
+ * 
+ * @author dennis.markmann
+ * @since JDK.1.7.0_25
+ * @version 1.0
+ */
+
+public class ExceptionLoggerTest extends TestCase {
+
+	private ExceptionLogger logger;
+
+	@Override
+	@Before
+	public void setUp() throws Exception {
+		this.logger = new ExceptionLogger();
+	}
+
+	@Test
+	public void testExceptionLogging() {
+		ExceptionLoggerTest.assertFalse(this.logger
+				.logException(new SuperException(0, "TestException",
+						"JustTesting")));
+	}
+}
