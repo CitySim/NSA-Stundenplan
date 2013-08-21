087133230334cd823e6c89c36737ef1928e8bbec
leonard- <wark@freenet.de>
2013-08-21 11:17:46 +0200

    dump it

--- server/src/test/server/suite/AllTests.java
+++ server/src/test/server/suite/AllTests.java
@@ -11,13 +11,12 @@ import server.operations.FilePrinterTest;
 import server.operations.NewsLetterHandlerTest;
 import server.operations.PasswordValidatorTest;
 import server.persistence.PersistEntityTest;
-import server.webservices.JSONTest;
 
 @RunWith(Suite.class)
 @SuiteClasses({ AccountHandlerTest.class, CookieHandlerTest.class,
 		ExceptionLoggerTest.class, FilePrinterTest.class,
 		NewsLetterHandlerTest.class, PasswordValidatorTest.class,
-		PersistEntityTest.class, JSONTest.class })
+		PersistEntityTest.class})
 public class AllTests {
 
 }
