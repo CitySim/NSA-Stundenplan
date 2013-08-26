d493009188b76fa9afb89d33947bfb8207fde64c
leonard- <wark@freenet.de>
2013-08-26 23:26:47 +0200

    available

--- server/src/java/server/resources/AvailableResource.java
+++ server/src/java/server/resources/AvailableResource.java
@@ -1,5 +1,38 @@
 package server.resources;
 
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import server.entities.Room;
+import server.entities.Teacher;
+
+import com.google.gson.Gson;
+
+@Path("available")
 public class AvailableResource {
+	
+	// FIXME Json richtig zusammen setzten, querys implementieren
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public String getAvailable() {
+		final Gson gson = new Gson();
+		String json = gson.toJson(getAvailableTeachers());
+		json = json + gson.toJson(getAvailableRooms());
+		return json;
+	}
+
+	private List<Teacher> getAvailableTeachers() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	private List<Room> getAvailableRooms() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
