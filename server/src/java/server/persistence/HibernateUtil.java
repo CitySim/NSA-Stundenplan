8a91c09eb57d84c81f3eba0269ad57dc7f69c42c
DennisMarkmann <dennismarkmann@hotmail.de>
2013-08-26 17:21:11 +0200

    +

--- server/src/java/server/persistence/HibernateUtil.java
+++ server/src/java/server/persistence/HibernateUtil.java
@@ -1,10 +1,5 @@
 package server.persistence;
 
-import java.sql.Connection;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.sql.Statement;
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -17,13 +12,14 @@ public class HibernateUtil {
 
 	static {
 		if (emf == null) {
-			HibernateUtil.checkDatabase();
-			final boolean exists = true;
-			if (exists) {
-				emf = Persistence.createEntityManagerFactory("nsa-stundenplan");
-			} else {
-				emf = Persistence.createEntityManagerFactory("nsa-stundenplan-new");
-			}
+			// HibernateUtil.checkDatabase();
+			// final boolean exists = true;
+			// if (exists) {
+			emf = Persistence.createEntityManagerFactory("nsa-stundenplan");
+			// } else {
+			// emf =
+			// Persistence.createEntityManagerFactory("nsa-stundenplan-new");
+			// }
 		}
 
 		if (entityManager == null) {
@@ -35,20 +31,20 @@ public class HibernateUtil {
 		return entityManager;
 	}
 
-	public static void checkDatabase() {
-		// TODO CHECK IF DB final EXISTS
-		final Connection con = null;
-		Statement stmt = null;
-		try {
-			stmt = con.createStatement();
-		} catch (final SQLException e) {
-			e.printStackTrace();
-		}
-		try {
-			final ResultSet rs = stmt.executeQuery("SELECT * FROM test");
-			System.out.println(rs.last());
-		} catch (final SQLException e) {
-			e.printStackTrace();
-		}
-	}
+	// public static void checkDatabase() {
+	// // TODO CHECK IF DB final EXISTS
+	// final Connection con = null;
+	// Statement stmt = null;
+	// try {
+	// stmt = con.createStatement();
+	// } catch (final SQLException e) {
+	// e.printStackTrace();
+	// }
+	// try {
+	// final ResultSet rs = stmt.executeQuery("SELECT * FROM test");
+	// System.out.println(rs.last());
+	// } catch (final SQLException e) {
+	// e.printStackTrace();
+	// }
+	// }
 }
