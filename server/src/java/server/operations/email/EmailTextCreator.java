f9866e6e0ad319b7dc00751a0208f0f4e5280421
DennisMarkmann <dennismarkmann@hotmail.de>
2013-08-27 22:57:26 +0200

    Fixed confirmationMail text. Updated TODO.

--- server/src/java/server/operations/email/EmailTextCreator.java
+++ server/src/java/server/operations/email/EmailTextCreator.java
@@ -25,6 +25,7 @@ class EmailTextCreator {
 		sb.append("hat sich eine Abweichung ergeben.");
 		sb.append(System.lineSeparator());
 
+		sb.append(System.lineSeparator());
 		sb.append("Betroffene Stunde(n) [Font größer]");
 		sb.append("Stunde | Fach | Lehrer");
 		sb.append("alt:");
@@ -34,11 +35,17 @@ class EmailTextCreator {
 		sb.append("Änderungsart: system.get (?) Ausfall / Vertretung / Verschiebung");
 		sb.append(System.lineSeparator());
 		sb.append("Hinweis: Hierbei handelt es sich um eine einmalige Änderung.");
+		sb.append(System.lineSeparator());
 		sb.append("Diese hat keinerlei Einfluss auf den regulären Stundenplan.");
+		sb.append(System.lineSeparator());
 		sb.append("Bitte beachten Sie, dass sich weitere Anpassungen ergeben könnten.");
+		sb.append(System.lineSeparator());
 		sb.append("Sie werden in diesem Fall erneut von uns benachrichtig.");
 		sb.append(System.lineSeparator());
+
+		sb.append(System.lineSeparator());
 		sb.append("Wir entschuldigen uns vielmals für die Unannehmlichkeiten.");
+		sb.append(System.lineSeparator());
 		sb.append("Ihr National Studenplan Agency Team.");
 
 		return sb.toString();
@@ -49,17 +56,25 @@ class EmailTextCreator {
 		final StringBuilder sb = new StringBuilder();
 
 		sb.append("Hallo,");
+		sb.append(System.lineSeparator());
 		sb.append("sie haben sich für den Newsletter der Nation Stundenplan Agency registriert.");
+		sb.append(System.lineSeparator());
 		sb.append("Die für sie eingetragene Klasse ist die " + form.getDescription() + " .");
 		sb.append(System.lineSeparator());
 
+		sb.append(System.lineSeparator());
 		sb.append("Bitte bestätigen sie ihre Anmeldung indem Sie auf den folgenden Link klicken:");
+		sb.append(System.lineSeparator());
 		sb.append(new NewsLetterHandler().generateRegistrationLink(form, eMailAddress));
 		sb.append(System.lineSeparator());
 
+		sb.append(System.lineSeparator());
 		sb.append("Sollten Sie die Registrierung nicht durchgeführt haben oder keine Newsletter erhalten wollen,");
+		sb.append(System.lineSeparator());
 		sb.append("ignorieren diese Nachricht.");
+		sb.append(System.lineSeparator());
 		sb.append("Ihre Addresse wird dann in kürze wieder gelöscht.");
+		sb.append(System.lineSeparator());
 
 		sb.append(System.lineSeparator());
 		sb.append("Ihr National Studenplan Agency Team.");
@@ -72,6 +87,7 @@ class EmailTextCreator {
 		final StringBuilder sb = new StringBuilder();
 
 		sb.append("Hallo,");
+		sb.append(System.lineSeparator());
 		sb.append("sie wurden als Administrator der Nation Stundenplan Agency registriert.");
 		sb.append(System.lineSeparator());
 
@@ -95,6 +111,7 @@ class EmailTextCreator {
 		final StringBuilder sb = new StringBuilder();
 
 		sb.append("Hallo,");
+		sb.append(System.lineSeparator());
 		sb.append("ihr passwort wurde erfolgreich geändert.");
 		sb.append(System.lineSeparator());
 
@@ -109,15 +126,18 @@ class EmailTextCreator {
 		return sb.toString();
 	}
 
-	public String generateRemoveRegistrationText(Newsletter newsletter) {
+	public String generateRemoveRegistrationText(final Newsletter newsletter) {
 
 		final StringBuilder sb = new StringBuilder();
 
 		sb.append("Hallo,");
+		sb.append(System.lineSeparator());
 		sb.append("um sich von dem Newsletter von der Klasse " + newsletter.getForm().getDescription() + "  abzumelden klicken Sie folgenden Link:");
 		sb.append(System.lineSeparator());
 
+		sb.append(System.lineSeparator());
 		sb.append(new NewsLetterHandler().generateRemoveLink(newsletter));
+		sb.append(System.lineSeparator());
 
 		sb.append(System.lineSeparator());
 		sb.append("Ihr National Studenplan Agency Team.");
