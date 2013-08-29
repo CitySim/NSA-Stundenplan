197882f73c7168e6699bf1014c7dce3021b9d97d
Sven Tatter <sven31894@gmail.com>
2013-08-29 20:14:03 +0200

    deserializer ist keine entity

--- server/src/java/server/entities/ReplacementDeserializer.java
+++ server/src/java/server/entities/ReplacementDeserializer.java
@@ -3,14 +3,12 @@ package server.entities;
 import java.lang.reflect.Type;
 import java.util.Date;
 
-import javax.persistence.Entity;
 import com.google.gson.JsonDeserializationContext;
 import com.google.gson.JsonDeserializer;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
 import com.google.gson.JsonParseException;
 
-@Entity
 public class ReplacementDeserializer implements JsonDeserializer<Replacement> {
 
 	private static final long serialVersionUID = 1L;
