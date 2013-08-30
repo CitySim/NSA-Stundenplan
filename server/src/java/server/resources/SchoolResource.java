147b0fa3bf128a67af351b8ea311bdf270e38859
leonard- <wark@freenet.de>
2013-08-30 09:15:06 +0200

    school moved in DB

--- server/src/java/server/resources/SchoolResource.java
+++ server/src/java/server/resources/SchoolResource.java
@@ -5,6 +5,9 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
+import server.entities.School;
+import server.persistence.HibernateUtil;
+
 import com.google.gson.Gson;
 
 @Path("school")
@@ -15,20 +18,11 @@ public class SchoolResource {
 	@Produces(MediaType.APPLICATION_JSON)
 	public String getFormsJSON() {
 		Gson gson = new Gson();
-		String json = gson.toJson(school);
+		String json = gson.toJson(getSchool());
 		return json;
 	}
 
-	// TODO: Dummy muss in ie Datenbank
-	private class School {
-		@SuppressWarnings("unused")
-		String image = "http://mw2.google.com/mw-panoramio/photos/medium/24678566.jpg";
-		@SuppressWarnings("unused")
-		String text = "++++ Neuer Rekord: Die IT1a hat den besten Notenschnitt, den jemals eine Klasse erreicht hat ++++<br>"
-				+ "++++ Zeugnisse sind fertig ++++<br>"
-				+ "++++ Studie: Killerspiele fördern Aufmerksamkeit und Reaktionsvermögen ++++<br>"
-				+ "++++ Folgende Lehrer sind krank: ... ++++<br>"
-				+ "++++ Stundenausfall für die Klassen IT3C, CH3M, IN5T ++++<br>"
-				+ "++++ Studie: Lernortverlegung nach Zuhause führt zu effektiverem Lernverhalten ++++<br>";
+	private School getSchool() {
+		return HibernateUtil.getEntityManager().find(School.class, 1);
 	}
 }
